011fda049504881fc85e1868c186e11eff1abc5b
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-02-28 01:39:40 -0300

    chore: adding fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import "@fontsource/albert-sans/400.css";
+import "@fontsource/albert-sans/600.css";
+import "@fontsource/albert-sans/700.css";
+import "@fontsource/ibarra-real-nova/400.css";
+import "@fontsource/ibarra-real-nova/600.css";
+import "@fontsource/ibarra-real-nova/700.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
